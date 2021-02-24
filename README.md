927362b15d09e79d2b0474ce5a903cbacbf40f40
stevesch <stephen.schlueter@blippar.com>
2021-02-24 07:19:58 -0800

    updated example; added README

---
+++ README.md
@@ -0,0 +1,25 @@
+# stevesch-WiFiConnector
+
+Provides simplified WiFi Manager and over-the-air update setup (sketch basically just needs to call setup and loop for the library).
+
+examples/minimal provides a simple sketch that loops and outputs status (current SSID, IP, etc.) on the serial port.
+
+![Example Screencap](examples/minimal/example-minimal-screencap.jpg)
+
+# Building and Running
+
+To get up-and-running:
+- PlatformIO is recommended
+- Build this library (builds its own minimal example)
+- Upload to any ESP32 board
+- When your board is booted, a WiFi config portal named "PageHandler-Test" should be available.
+- Connecting to the "PageHandler-Test" WiFi network with any device and set the config options for the WiFi network of your router (choose your network/SSID and set the password).
+- Connect to your router's WiFi network and open the ESP32 page in any browser.  The IP of the device will be displayed in the serial output if you're monitoring serial output of the board.  If your router supports mDNS, you should be able to open
+
+"http://PageHandler-Test.local/"
+
+(otherwise you'll need to open, e.g.
+
+"http://192.168.x.x" or "http://10.x.x.x"
+
+where x.x/x.x.x matches the address displayed in the serial output from your board).
--- examples/minimal/minimal.ino
+++ examples/minimal/minimal.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <pgmspace.h>   // for PROGMEM
 #include <ESPAsyncWebServer.h>
 
 #include <stevesch-WiFiConnector.h>
@@ -11,12 +12,18 @@ void printWiFiStaus();
 
 AsyncWebServer server(80);
 
+void handleIndex(AsyncWebServerRequest *request);         // defined below
+void handlePageNotFound(AsyncWebServerRequest* request);  // defined below
+
 void setup()
 {
   Serial.begin(115200);
   while (!Serial);
 
-  stevesch::WiFiConnector::setup(&server);
+  stevesch::WiFiConnector::setup(&server, "WiFiConnector-Test");
+
+  server.on("/", HTTP_GET, handleIndex);
+  server.onNotFound(handlePageNotFound);
   server.begin();
 }
 
@@ -47,4 +54,39 @@ void printWiFiStaus()
   if (stevesch::WiFiConnector::isUpdating()) {
     Serial.println("OTA update is being performed");
   }
+}
+
+
+const char kSimplePage[] PROGMEM = R"delim(
+<!DOCTYPE HTML>
+<html><head><title>WiFiConnector Test Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+html { font-family: Verdana; display: inline-block; text-align: center; }
+body { margin: 0; }
+p { font-size: 1.2rem; }
+.titlebar { overflow: hidden; background-color: #29a64f; color: white; font-size: 0.6rem; }
+</style></head><body>
+<div class="titlebar"><h1>WiFiConnector Test</h1></div>
+<div><p>SSID: %SSID_FIELD%</p></div>
+<div><p>Name: %NAME_FIELD%</p></div>
+<div><p>IP: %IP_FIELD%</p></div>
+</body></html>)delim";
+
+void handleIndex(AsyncWebServerRequest *request)
+{
+  String strSsid(WiFi.SSID());
+  String strName(WiFi.getHostname());
+  String strIp(WiFi.localIP().toString());
+  String str(kSimplePage);
+  // Note: these fields are replaced once (only upon initial page load):
+  str.replace("%SSID_FIELD%", strSsid.c_str());
+  str.replace("%NAME_FIELD%", strName.c_str());
+  str.replace("%IP_FIELD%", strIp.c_str());
+  request->send(200, "text/html", str);
+}
+
+void handlePageNotFound(AsyncWebServerRequest* request) {
+  Serial.println("Callback: handlePageNotFound");
+  request->redirect("/");
 }
